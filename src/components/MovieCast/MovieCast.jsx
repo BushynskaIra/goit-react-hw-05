311058a6c3cb4e8dd7d8a10f4ae8b198b83eafab
BushynskaIra <bushynskaira@gmail.com>
2024-08-21 13:45:15 -0600

    update URL

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -11,7 +11,7 @@ function MovieCast() {
     axios
       .get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
         headers: {
-          Authorization: 'Bearer YOUR_API_KEY',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDRjN2Q5YTFmMzRiNzM0ZTU5YmRlZmNjMzI0M2ZlZCIsIm5iZiI6MTcyNDE4NjM3NC4yMjUwNDYsInN1YiI6IjY2YmZiNjJlNTA1MzJkYWIzZDQ4OWNhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aLLl57GGpRPEiyBKqKLYLZlz_-CpJxhFrFTLGWaSHmc',
         },
       })
       .then(response => setCast(response.data.cast))
